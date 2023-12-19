da48c1163898b717360a4ec7be164c2aecf8d846
dincherselimov <dinchitoo@gmail.com>
2023-12-19 15:55:57 +0200

    changes-to-viewport

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -12,10 +12,6 @@ import "@/styles/home/motivation.css";
 import "@/styles/home/form.css";
 import type { AppProps } from "next/app";
 
-
-
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
-
-
