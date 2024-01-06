01353b1f3a06bad29ad24e628e740940915ebba0
dincherselimov <dinchitoo@gmail.com>
2024-01-06 22:31:50 +0200

    recover sendEmail

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -11,14 +11,13 @@ import "@/styles/home/profSkills.css";
 import "@/styles/home/motivation.css";
 import "@/styles/home/form.css";
 import type { AppProps } from "next/app";
-import Head from "next/head"; 
+import Head from "next/head";
 import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
-
   return (
     <>
-    <Script
+      <Script
         strategy="lazyOnload"
         src={`https://www.googletagmanager.com/gtag/js?id=G-Z10EFQ0Q61`}
       />
@@ -33,10 +32,13 @@ export default function App({ Component, pageProps }: AppProps) {
                     });
                 `}
       </Script>
-      
+
       <Head>
         <meta property="og:title" content="Dincher Selimov - Web Developer" />
-        <meta property="og:description" content="Explore Dincher Selimov's expertise in web development, including front-end and back-end technologies, responsive design, and more." />
+        <meta
+          property="og:description"
+          content="Explore Dincher Selimov's expertise in web development, including front-end and back-end technologies, responsive design, and more."
+        />
         <meta property="og:image" content="/me.jpg" />
         <meta name="og:image:width" content="200" />
         <meta name="og:image:height" content="200" />
