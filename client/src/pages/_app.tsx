07c3551f139da9dea777c29c197cb97c24d47dd0
dincherselimov <dinchitoo@gmail.com>
2024-01-06 16:12:31 +0200

    og-image

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -11,7 +11,7 @@ import "@/styles/home/profSkills.css";
 import "@/styles/home/motivation.css";
 import "@/styles/home/form.css";
 import type { AppProps } from "next/app";
-import { Head } from "next/document";
+import Head from "next/head"; 
 
 export default function App({ Component, pageProps }: AppProps) {
 
